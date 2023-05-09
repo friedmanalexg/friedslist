7039280dae5845fbb47a52f242d3710711099b7c
Alex <friedmanalexg@gmail.com>
2023-05-09 16:38:29 -0400

    true routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { MyListingsComponent } from './my-listings/my-listings.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 
 const routes: Routes = [
-  {path: 'listings', component: ListingsPageComponent, pathMatch: 'full'}
+  {path: '', redirectTo: '/listings', pathMatch: 'full'},
+  {path: 'listings', component: ListingsPageComponent, pathMatch: 'full'},
+  {path: 'listings/:id', component: ListingDetailPageComponent},
+  {path: 'contact/:id', component: ContactPageComponent},
+  {path: 'edit-listing/:id', component: EditListingPageComponent},
+  {path: 'my-listings', component: MyListingsComponent},
+  {path: 'new-listing', component: NewListingPageComponent}
+
 ];
 
 @NgModule({
