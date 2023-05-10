3193dca3bb608b29efc3252344f0ee7ab93987b1
Alex <friedmanalexg@gmail.com>
2023-05-10 15:54:34 -0400

    listings component implemented

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Listing } from '../types'; 
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-listings-page',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./listings-page.component.css']
 })
 export class ListingsPageComponent {
+  listings: Listing[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.listings = fakeListings;
+  }
 
 }
